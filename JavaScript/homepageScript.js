00dcd1a5fb786686dc446966dfe2fdc1dc1eab26
KarthikS <karthiksgujjar@gmail.com>
2023-06-20 16:02:13 +0530

    Bug fixes in JavaScript file and earth.html

--- JavaScript/homepageScript.js
+++ JavaScript/homepageScript.js
@@ -6,11 +6,11 @@ let i=0;
 hamBtn.addEventListener('click', () => {
     navBar.classList.toggle('active');
     if(i%2 === 0){
-        hamImg.src = "../Images/Icons/hamMenuClose.png";
+        hamImg.src = "Images/Icons/hamMenuClose.png"; //Here relative path is used because hamImg.src is setting the path to index.html file and index.html is at root. So Images/Icons/hamMenuClose.png can be used. 
         i++;
     }
     else{
-        hamImg.src = "../Images/Icons/hamMenu.png";
+        hamImg.src = "Images/Icons/hamMenu.png"; // Same as above.
         i++;
     }
 });
